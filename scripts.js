381476a2ced0dac8c61af53163624d6a9805dbb0
Simon <47718189+luo-simon@users.noreply.github.com>
2020-08-02 14:12:53 +0100

    Fix resolutions

--- scripts.js
+++ scripts.js
@@ -8,10 +8,10 @@ document.querySelector("select").onchange = function() {
     } else if (this.value === "rift"){
         game.innerHTML = '<iframe frameborder="0" src="https://itch.io/embed-upload/1982902?color=161616" allowfullscreen="" width="980" height="680"><a href="https://itsnotsimon.itch.io/rift">Play ~ Rift ~ on itch.io</a></iframe>';
     } else if (this.value === "ex-umbris"){
-        game.innerHTML = '<iframe frameborder="0" src="https://itch.io/embed-upload/2482538?color=161616" allowfullscreen="" width="1600" height="860"><a href="https://itsnotsimon.itch.io/ex-umbris">Play Ex Umbris on itch.io</a></iframe>';
+        game.innerHTML = '<iframe frameborder="0" src="https://itch.io/embed-upload/2482538?color=161616" width="1042" height="610"><a href="https://itsnotsimon.itch.io/ex-umbris">Play Ex Umbris on itch.io</a></iframe>';
     } else if (this.value === "beyond-eden"){
         game.innerHTML = '<iframe frameborder="0" src="https://itch.io/embed-upload/1742335?color=161616" allowfullscreen="" width="1600" height="860"><a href="https://itsnotsimon.itch.io/beyond-eden">Play Beyond Eden on itch.io</a></iframe>';
     } else if (this.value === "the-cave"){
-        game.innerHTML = '<iframe frameborder="0" src="https://itch.io/embed-upload/1038065?color=161616" allowfullscreen="" width="1600" height="860"><a href="https://itsnotsimon.itch.io/the-cave">Play The Cave on itch.io</a></iframe>';
+        game.innerHTML = '<iframe frameborder="0" src="https://itch.io/embed-upload/1038065?color=161616" allowfullscreen="" width="980" height="680"><a href="https://itsnotsimon.itch.io/the-cave">Play The Cave on itch.io</a></iframe>';
     }
 }
